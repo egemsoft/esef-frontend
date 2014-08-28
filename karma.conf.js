0ccab65da7d57af4694743649b6034d9b5c3f8c9
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-08-28 14:21:49 +0300

    spec tests imports fixed

--- karma.conf.js
+++ karma.conf.js
@@ -13,8 +13,9 @@ module.exports = function (config) {
         files: [
           'bower_components/angular/angular.js',
           'bower_components/angular-mocks/angular-mocks.js',
-          'src/scripts/*.js',
-          'src/scripts/*/**.js',
+          'src/*.js',
+          'src/*/scripts/*.js',
+          'src/*/scripts/**/*.js',
           'test/spec/**/*.js'
         ],
 
