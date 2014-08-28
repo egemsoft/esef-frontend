dc9c23311febe6313f60c166f5b1ad7213a6a3e4
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-08-28 14:03:28 +0300

    pagination app fix

---
+++ src/pagination/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name esef.frontend.pagination
+ * @description
+ * Pagination module for esef-frontend. Provides pagination service.
+ * @function
+ * @author İsmail Demirbilek
+ */
+angular.module('esef.frontend.pagination', []);
